1e7931a28e8a2dda49c48e59d52f3826d698dd17
AAli <116252418+Aaliya-Shanez@users.noreply.github.com>
2023-04-28 09:55:14 +0530

    Add files via upload

--- Cpus.java
+++ Cpus.java
@@ -1,43 +1,33 @@
 import java.util.*;
-class Cpu
-{
-int price=3400;
-class Processor
-{
-int noofcores=5;
-String manufacture="Intel Corp";
-void display()
-{
-System.out.println("No.of cores:" +noofcores);
-System.out.println("Manufacture:" +manufacture);
- }
- }
- void display()
-{
- Processor p=new Processor();
-  p.display();
- System.out.println("Price :" +price);
- }
- static class Ram
- {
- int memory =8;
- String manufacture="Info Technology";
- void display()
- {
- System.out.println("Memory :" +memory);
- System.out.println("Manufacture :" +manufacture);
- }
- }
- }
-  public class Cpus
- {
-  public static void main(String args[])
- {
-Cpu c1=new Cpu();
+class Cpu {
+int price=34000;
+class processor {
+int noofCores=4;
+String manufactures="intel corp";
+void display() {
+System.out.println("noofCores:"+ noofCores);
+System.out.println("manufacture:"+ manufactures);
+}
+}
+void display(){
+processor p= new processor();
+p.display();
+System.out.println("price:"+ price);
+}
+static class Ram{
+int memory =8;
+String manufacture="intercode";
+void display(){
+System.out.println("memory"+ memory);
+System.out.println("manufacture:"+ manufacture);
+}
+}
+}
+class Cpus{
+public static void main (String[] args){
+Cpu c1 = new Cpu();
 c1.display();
-Cpu.Ram r1=new Cpu.Ram();
+Cpu.Ram r1 =new Cpu.Ram();
 r1.display();
 }
 }
-
-
